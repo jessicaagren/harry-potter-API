72bb0f950ff419befe16e5f67ba07fa692cbfedc
jessicaagren <jessicaagren@hotmail.com>
2025-01-10 10:50:29 +0100

    Rensat css och börjat litegrann på main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import './style.css'
+
+const HPurl = `https://hp-api.onrender.com/api/characters`;
+
+const getAllPeopleInACategory = async (category: string) => {
+  const allPeopleURL = `${HPurl}/${category}`
+  const response = await fetch(allPeopleURL);
+  const data = response.json();
+  return data;
+}
+
+console.log(await getAllPeopleInACategory(`students`));
+console.log(await getAllPeopleInACategory(`staff`));
